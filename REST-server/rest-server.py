6590dcaed5f6a94b1a29232af688e073f8333f93
BryanTurns <95263942+BryanTurns@users.noreply.github.com>
2024-11-19 15:20:42 -0700

    Added skeleton route for checking status

--- REST-server/rest-server.py
+++ REST-server/rest-server.py
@@ -2,6 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+@app.route("/api/uploadVideo")
+def chop_video():
+    return "<p>Uploading</p>"
+
+@app.route("/api/checkStatus")
+def check_status():
+    return "Checking status"
+
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
