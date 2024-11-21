12eeac3364719a74b8a3fd40e0c14f3fb059ef0b
Bryan Turns <turnsbryan@gmail.com>
2024-11-20 17:41:29 -0700

    Added grpc for frame-chopper.py and made redis service have static IP. Also rearranged directory structure to create a utils package.

--- workers/frame-chopper/frame-chopper.py
+++ workers/frame-chopper/frame-chopper.py
@@ -1,23 +1,41 @@
-import cv2, hashlib, os
-from time import sleep
+import cv2, os, grpc, redis
+from concurrent import futures
+from eztopo_utils.chopper import chopper_pb2, chopper_pb2_grpc
+from minio import Minio
+from eztopo_utils.constants import FRAME_TIME_INTERVAL
 
 
-LOCAL = True
-LOCAL_VID_PATH = "../../local-data/input/bouldering.MP4"
-LOCAL_FRAME_PATH = "../../local-data/frames/"
 # Number of seconds between saved frames
-FRAME_TIME_INTERVAL = 2
 
-def main():
-    while True:
-        if LOCAL:
-            vidObj = getVideoLocal(LOCAL_VID_PATH)
-            vidHash = generate_fingerprint(LOCAL_VID_PATH, 10)
-            files = os.listdir(LOCAL_FRAME_PATH)
-            for file in files:
-                os.remove(f"./{LOCAL_FRAME_PATH}/{file}")
 
-        fps = int(vidObj.get(cv2.CAP_PROP_FPS))
+# IP is static (refer to /message-queue/redis-service.yaml)
+redisClient = redis.Redis(host="10.108.148.45", port=6379)
+
+
+minioHost = os.getenv("MINIO_HOST") or "minio-proj.minio-ns.svc.cluster.local:9000"
+minioUser = os.getenv("MINIO_USER") or "rootuser"
+minioPasswd = os.getenv("MINIO_PASSWD") or "rootpass123"
+minioClient = Minio(minioHost,
+               secure=False,
+               access_key=minioUser,
+               secret_key=minioPasswd)
+
+
+class ChopperServicer(chopper_pb2_grpc.chopperServicer):
+    def Chop(self, request, context):
+        dataFiles = os.listdir("./data")
+        for file in dataFiles:
+            os.remove(f"./data/{file}")
+
+        videoHash = request.hash
+        originalStatus = redisClient.get(videoHash)
+        # Check if the video is ready to chop
+        # if originalStatus != SOME_CONSTANT:
+        #     err
+        video = minioClient.get_object("eztopo-bucket", f"original-{videoHash}.mp4", "./data/input.mp4")
+        videoObject = cv2.VideoCapture("./input.mp4")
+
+        fps = int(videoObject.get(cv2.CAP_PROP_FPS))
         # Every frameCaptureInteval, save a frame
         frameCaptureInterval = int(FRAME_TIME_INTERVAL * fps)
         # Number of frames read
@@ -25,48 +43,29 @@ def main():
         success = True
         while success:
             # Read a frame
-            success, image = vidObj.read()
+            success, image = videoObject.read()
             # Every frameCaptureInterval, save the frame
             if (n % frameCaptureInterval) == 0:
-                if LOCAL:
-                    savePath = LOCAL_FRAME_PATH + f"{vidHash}-frame{n // frameCaptureInterval}.jpg"
-                    saveVideoLocal(savePath, image)
+                frameNumber = n // frameCaptureInterval
+                savePath = f"./data/frame-{frameNumber}-{videoHash}.jpg"
+                # Consider changing img quality in params
+                cv2.imwrite(savePath, image)
             n = n + 1
+        
+        # Clear files from data for next call
+        dataFiles = os.listdir("./data")
+        for file in dataFiles:
+            os.remove(f"./data/{file}")
+        
 
+def grpc_main():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    chopper_pb2_grpc.add_chopperServicer_to_server(ChopperServicer(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    server.wait_for_termination()
 
-        if LOCAL:
-            break
-
-
-def saveVideoLocal(path, img):
-    cv2.imwrite(path, img)
-
-
-def getVideoLocal(path):
-    vidObj = cv2.VideoCapture(path)
-
-    return vidObj
-
-
-# Generated from chatgpt
-# Generates hash based on first n chunks. If n < 0 then do the whole file
-def generate_fingerprint(file_path, n):
-    hasher = hashlib.sha256()
-    with open(file_path, 'rb') as file:
-        if n > 0:
-            for i in range(n):
-                chunk = file.read(4096)  # Read file in chunks
-                if not chunk:
-                    break
-                hasher.update(chunk)
-        else:
-            while True:
-                chunk = file.read(4096)  # Read file in chunks
-                if not chunk:
-                    break
-                hasher.update(chunk)
-    return hasher.hexdigest()
 
 if __name__ == "__main__":
-    main()
+    grpc_main()
 
