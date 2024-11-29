3a3d98ce2f6caf6a0fef48b049a3e322fc04a507
BryanTurns <95263942+BryanTurns@users.noreply.github.com>
2024-11-28 17:48:00 -0700

    Frame chopper can now download vid and upload chopped frames

--- data-processing/video-chopper/video-chopper.py
+++ data-processing/video-chopper/video-chopper.py
@@ -1,13 +1,16 @@
 import cv2, os, redis
 from google.cloud import storage
 from dotenv import load_dotenv
+import threading
 
 print("Loading environment variables")
 load_dotenv("../../.env")
 constants = {"BUCKET_NAME": os.getenv("BUCKET_NAME"),
              "DOWNLOAD_PATH": "./data/input",
              "UPLOAD_PATH": "./data/output",
-             "FRAME_TIME_INTERVAL": 2}
+             "FRAME_TIME_INTERVAL": 2,
+             "CHOPPING": os.getenv("CHOPPING"),
+             "CHOPPED": os.getenv("CHOPPED")}
 
 print("Initiating Redis")
 redisClient = redis.Redis(host="localhost", port=6379)
@@ -21,12 +24,13 @@ def  main():
     while True:
         print("Waiting for work...")
         uuid = redisClient.blpop("chopQueue")[1].decode()
-        print("Working!")
+        redisClient.set(uuid, constants["CHOPPING"])
+        print(f"Working on {uuid}")
 
         blobName = f"Upload-{uuid}"
         blob = bucket.blob(blobName)
         blob.download_to_filename(f"{constants['DOWNLOAD_PATH']}/{blobName}")
-
+        
         videoObject = cv2.VideoCapture(f"{constants['DOWNLOAD_PATH']}/{blobName}")
         fps = int(videoObject.get(cv2.CAP_PROP_FPS))
         # Every frameCaptureInteval seconds, save a frame
@@ -40,18 +44,26 @@ def  main():
                 savePath = f"{constants['UPLOAD_PATH']}/frame-{frameNumber}-{uuid}.jpg"
                 cv2.imwrite(savePath, image)
             n = n + 1
+        framesCapturedCount = n // frameCaptureInterval
         
-        dataFiles = os.listdir(constants["DOWNLOAD_PATH"])
-        for filename in dataFiles:
+        for filename in os.listdir(constants["DOWNLOAD_PATH"]):
             os.remove(f"{constants['DOWNLOAD_PATH']}/{filename}")
-
         
-        
-        
+        threading.Thread(target=upload_frames_to_GCP, args=(uuid, framesCapturedCount)).start()
 
 
+def upload_frames_to_GCP(uuid, framesCapturedCount):
+    print(f"Uploading frames for {uuid}")
+    for frameNumber in range(framesCapturedCount):
+        blobName = f"frame-{frameNumber}-{uuid}.jpg"
+        blob = bucket.blob(blobName)
+        blob.upload_from_filename(f"{constants['UPLOAD_PATH']}/{blobName}")
+    
+    for filename in os.listdir(constants["UPLOAD_PATH"]):
+        os.remove(f"{constants['UPLOAD_PATH']}/{filename}")
 
-
+    redisClient.set(uuid, constants["CHOPPED"])
+    redisClient.rpush("predictorQueue", uuid)
     
 
 
