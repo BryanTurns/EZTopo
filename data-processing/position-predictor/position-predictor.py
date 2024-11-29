149a2e10b52a812a684b3b4c9b4cfb805b05d932
BryanTurns <95263942+BryanTurns@users.noreply.github.com>
2024-11-28 18:03:49 -0700

    predictor waits for queue

--- data-processing/position-predictor/position-predictor.py
+++ data-processing/position-predictor/position-predictor.py
@@ -1,8 +1,32 @@
 from ultralytics import YOLO
-from eztopo_utils.constants import MODEL_SIZE
-import cv2
+from dotenv import load_dotenv
+from google.cloud import storage
+import cv2, os, redis
+
+
+print("Loading environment variables")
+load_dotenv("../../.env")
+constants = {"BUCKET_NAME": os.getenv("BUCKET_NAME"),
+             "DOWNLOAD_PATH": "./data/input",
+             "UPLOAD_PATH": "./data/output",
+             "FRAME_TIME_INTERVAL": 2,
+             "CHOPPING": os.getenv("CHOPPING"),
+             "CHOPPED": os.getenv("CHOPPED")}
+
+print("Initiating Redis")
+redisClient = redis.Redis(host="localhost", port=6379)
+
+print("Initiating storage")
+storage_client = storage.Client()
+bucket = storage_client.bucket(constants["BUCKET_NAME"])
+
+print("Loading model")
+model = YOLO(f"./models/yolo11s-pose.pt")
 
 def main():
-    model = YOLO(f"./models/yolo11{MODEL_SIZE}-pose.pt")
+    while True:
+        uuid = redisClient.blpop("predictorQueue")[1].decode()
 
+if __name__ == "__main__":
+    main()
     
