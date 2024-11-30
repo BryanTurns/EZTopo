f9bbbb41230dabe49519a8cd540912bc40b4513c
BryanTurns <95263942+BryanTurns@users.noreply.github.com>
2024-11-30 14:14:59 -0700

    Updated gitignore to ignore files in input/output but not the directories themselves

--- data-processing/output-generator/output-generator.py
+++ data-processing/output-generator/output-generator.py
@@ -21,20 +21,34 @@ def main():
     uuid = redisClient.blpop("outputQueue")[1].decode()
     print("Generating output!")
 
-    blobName = f"{uuid}.json"
-    json_filepath = f"{constants['DOWNLOAD_PATH']}/{blobName}"
-    blob = bucket.blob(blobName)
-    blob.download_to_filename(json_filepath)
+    jsonBlobName = f"{uuid}.json"
+    jsonFilepath = f"{constants['DOWNLOAD_PATH']}/{jsonBlobName}"
+    blob = bucket.blob(jsonBlobName)
+    blob.download_to_filename(jsonFilepath)
 
-    with open(json_filepath, "r") as json_file:
+    videoBlobName = f"Upload-{uuid}"
+    inputVideoFilepath = f"{constants['DOWNLOAD_PATH']}/{videoBlobName}"
+    blob = bucket.blob(videoBlobName)
+    blob.download_to_filename(inputVideoFilepath)
+
+    with open(jsonFilepath, "r") as json_file:
         position_data = json.load(json_file)
-    print(position_data)
 
+    inputVideoObject = cv2.VideoCapture(inputVideoFilepath)
+    fps = int(inputVideoObject.get(cv2.CAP_PROP_FPS))
+    dimensions = (int(inputVideoObject.get(cv2.CAP_PROP_FRAME_WIDTH)), int(videoObject.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+    outputVideoFilepath = f"{constants['UPLOAD_PATH']}/Output-{uuid}.mp4"
+    outputVideoObject = cv2.VideoWriter(outputVideoFilepath, cv2.VideoWriter_fourcc(*'mp4v'), fps, dimensions)
 
+    while True:
+        success, inputFrame = inputVideoObject.read()
+        if not success:
+            break
 
+        for i in range(len(position_data) - 1):
+            outputFrame = cv2.line(inputFrame, position_data[i], position_data[i+1], (0, 0, 255), 2)
+        outputVideoObject.write(outputFrame)
 
-
-    pass
 
 if __name__ == "__main__":
     main()
