0b3e5831f0a573b756ababa8ac967278d2e6ad7b
Bryan Turns <turnsbryan@gmail.com>
2024-12-08 11:13:24 -0700

    Thread pools added for video-chopper

--- data-processing/video-chopper/video-chopper.py
+++ data-processing/video-chopper/video-chopper.py
@@ -1,6 +1,6 @@
 import cv2, os, redis
 from google.cloud import storage
-import threading
+import concurrent.futures
 
 def getKey():
     with open("/etc/secret-volume/service-account", "r") as secretFile:
@@ -28,50 +28,53 @@ bucket = storage_client.bucket(constants["BUCKET_NAME"])
 
 
 def  main():
-    while True:
-        print("Waiting to chop...")
-        uuid = redisClient.blpop("chopQueue")[1].decode()
-        redisClient.set(uuid, constants["CHOPPING"])
-        print(f"Chopping {uuid}")
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        while True:
+            print("Waiting to chop...")
+            uuid = redisClient.blpop("chopQueue")[1].decode()
+            redisClient.set(uuid, constants["CHOPPING"])
+            print(f"Chopping {uuid}")
 
-        blobName = f"Upload-{uuid}"
-        blob = bucket.blob(blobName)
-        blob.download_to_filename(f"{constants['DOWNLOAD_PATH']}/{blobName}")
+            blobName = f"Upload-{uuid}"
+            blob = bucket.blob(blobName)
+            blob.download_to_filename(f"{constants['DOWNLOAD_PATH']}/{blobName}")
 
-        videoObject = cv2.VideoCapture(f"{constants['DOWNLOAD_PATH']}/{blobName}")
-        fps = int(videoObject.get(cv2.CAP_PROP_FPS))
-        # Every frameCaptureInteval seconds, save a frame
-        frameCaptureInterval = int(constants["FRAME_TIME_INTERVAL"] * fps)
-        n = 0
-        success = True
-        while success:
-            success, image = videoObject.read()
-            if (n % frameCaptureInterval) == 0:
-                frameNumber = n // frameCaptureInterval
-                savePath = f"{constants['UPLOAD_PATH']}/frame-{frameNumber}-{uuid}.jpg"
-                cv2.imwrite(savePath, image)
-            n = n + 1
-        videoObject.release()
-        framesCapturedCount = n // frameCaptureInterval
-        
-        for filename in os.listdir(constants["DOWNLOAD_PATH"]):
-            os.remove(f"{constants['DOWNLOAD_PATH']}/{filename}")
-        
-        threading.Thread(target=upload_frames_to_GCP, args=(uuid, framesCapturedCount)).start()
+            videoObject = cv2.VideoCapture(f"{constants['DOWNLOAD_PATH']}/{blobName}")
+            fps = int(videoObject.get(cv2.CAP_PROP_FPS))
+            # Every frameCaptureInteval seconds, save a frame
+            frameCaptureInterval = int(constants["FRAME_TIME_INTERVAL"] * fps)
+            n = 0
+            success = True
+            while success:
+                success, image = videoObject.read()
+                if (n % frameCaptureInterval) == 0:
+                    frameNumber = n // frameCaptureInterval
+                    savePath = f"{constants['UPLOAD_PATH']}/frame-{frameNumber}-{uuid}.jpg"
+                    cv2.imwrite(savePath, image)
+                n = n + 1
+            videoObject.release()
+            framesCapturedCount = n // frameCaptureInterval
+            
+            for filename in os.listdir(constants["DOWNLOAD_PATH"]):
+                os.remove(f"{constants['DOWNLOAD_PATH']}/{filename}")
+            
+            executor.submit(upload_frames_to_GCP, uuid, framesCapturedCount)
 
 
 def upload_frames_to_GCP(uuid, framesCapturedCount):
     print(f"Uploading frames for {uuid}")
+    upload_paths = []
     for frameNumber in range(framesCapturedCount):
         blobName = f"frame-{frameNumber}-{uuid}.jpg"
         blob = bucket.blob(blobName)
-        blob.upload_from_filename(f"{constants['UPLOAD_PATH']}/{blobName}")
+        upload_paths.append(f"{constants['UPLOAD_PATH']}/{blobName}")
+        blob.upload_from_filename(upload_paths[-1])
 
     redisClient.set(uuid, constants["CHOPPED"])
     redisClient.rpush("predictorQueue", f"{uuid} {framesCapturedCount}")
     
-    for filename in os.listdir(constants["UPLOAD_PATH"]):
-        os.remove(f"{constants['UPLOAD_PATH']}/{filename}")
+    for filepath in upload_paths:
+        os.remove(filepath)
 
 if __name__ == "__main__":
     main()
